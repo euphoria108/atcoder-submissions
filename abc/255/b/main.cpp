5a73084e5ecc5e1ac99f7b3fc0bef53e787abbb6
Daichi Yamazaki <yamazaki.d.zero@gmail.com>
2022-06-11 23:00:21 +0900

    Add ABC 255 A-C, D wa

---
+++ abc/255/b/main.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <numeric>
+#include <cmath>
+
+using namespace std;
+
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+
+/* debug */
+// 標準エラー出力を含む提出はrejectされる場合もあるので注意
+#define debug(x) cerr << "\033[33m(line:" << __LINE__ << ") " << #x << ": " << x << "\033[m" << endl;
+
+/* func */
+inline int in_int() {int x; cin >> x; return x;}
+inline ll in_ll() {ll x; cin >> x; return x;}
+inline string in_str() {string x; cin >> x; return x;}
+// search_length: 走査するベクトル長の上限(先頭から何要素目までを検索対象とするか、1始まりで)
+template <typename T> inline bool vector_finder(std::vector<T> vec, T element, unsigned int search_length) {
+    auto itr = std::find(vec.begin(), vec.end(), element);
+    size_t index = std::distance( vec.begin(), itr );
+    if (index == vec.size() || index >= search_length) {return false;} else {return true;}
+}
+template <typename T> inline void print(const vector<T>& v, string s = " ")
+    {rep(i, v.size()) cout << v[i] << (i != (ll)v.size() - 1 ? s : "\n");}
+template <typename T, typename S> inline void print(const pair<T, S>& p)
+    {cout << p.first << " " << p.second << endl;}
+template <typename T> inline void print(const T& x) {cout << x << "\n";}
+template <typename T, typename S> inline void print(const vector<pair<T, S>>& v)
+    {for (auto&& p : v) print(p);}
+// 第一引数と第二引数を比較し、第一引数(a)をより大きい/小さい値に上書き
+template <typename T> inline bool chmin(T& a, const T& b) {bool compare = a > b; if (a > b) a = b; return compare;}
+template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b; if (a < b) a = b; return compare;}
+
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+    vi A;
+    int buf;
+    rep(i, K){
+        cin >> buf;
+        A.pb(buf);
+    }
+    vi X, Y;
+    int bufb;
+    rep(i, N){
+        cin >> buf >> bufb;
+        X.pb(buf);
+        Y.pb(bufb);
+    }
+
+    double dist = 0;
+    rep(i, N){
+        if (find(A.begin(), A.end(), i+1) != A.end()) {
+            // cout << i+1 << "in A, continue." << endl;
+            continue;
+        }
+
+        ll x = X.at(i);
+        ll y = Y.at(i);
+        double min_dist = 1e9;
+        for (auto a : A) {
+            ll x_ = X.at(a-1);
+            ll y_ = Y.at(a-1);
+            ll tmp_sq_dist = (x-x_)*(x-x_) + (y-y_)*(y-y_);
+            double tmp_dist = sqrt(tmp_sq_dist);
+            cout.precision(15);
+            // cout << "tmp_dist: " << tmp_dist << endl;
+            min_dist = min(min_dist, tmp_dist);
+        }
+        dist = max(dist, min_dist);
+    }
+
+    cout << dist << endl;
+    return 0;
+
+}
---
+++ abc/255/d/wa.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <numeric>
+
+using namespace std;
+
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+
+/* debug */
+// 標準エラー出力を含む提出はrejectされる場合もあるので注意
+#define debug(x) cerr << "\033[33m(line:" << __LINE__ << ") " << #x << ": " << x << "\033[m" << endl;
+
+/* func */
+inline int in_int() {int x; cin >> x; return x;}
+inline ll in_ll() {ll x; cin >> x; return x;}
+inline string in_str() {string x; cin >> x; return x;}
+// search_length: 走査するベクトル長の上限(先頭から何要素目までを検索対象とするか、1始まりで)
+template <typename T> inline bool vector_finder(std::vector<T> vec, T element, unsigned int search_length) {
+    auto itr = std::find(vec.begin(), vec.end(), element);
+    size_t index = std::distance( vec.begin(), itr );
+    if (index == vec.size() || index >= search_length) {return false;} else {return true;}
+}
+template <typename T> inline void print(const vector<T>& v, string s = " ")
+    {rep(i, v.size()) cout << v[i] << (i != (ll)v.size() - 1 ? s : "\n");}
+template <typename T, typename S> inline void print(const pair<T, S>& p)
+    {cout << p.first << " " << p.second << endl;}
+template <typename T> inline void print(const T& x) {cout << x << "\n";}
+template <typename T, typename S> inline void print(const vector<pair<T, S>>& v)
+    {for (auto&& p : v) print(p);}
+// 第一引数と第二引数を比較し、第一引数(a)をより大きい/小さい値に上書き
+template <typename T> inline bool chmin(T& a, const T& b) {bool compare = a > b; if (a > b) a = b; return compare;}
+template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b; if (a < b) a = b; return compare;}
+
+
+int main() {
+    ll N, Q;
+    cin >> N >> Q;
+    vector<ull> A;
+    ull buf;
+    ull sum_a = 0;
+    ull max_a = 0;
+    ull min_a = 1e10;
+    rep(i, N){
+        cin >> buf;
+        A.pb(buf);
+        sum_a += buf;
+        max_a = max(buf, max_a);
+        min_a = min(buf, min_a);
+    }
+    vector<ull> X;
+    rep(i, Q){
+        cin >> buf;
+        X.pb(buf);
+    }
+
+    sort(A.begin(), A.end());
+    vector<ull> cumsum_a;
+    rep(i, N){
+        if(i>0){
+            cumsum_a.pb(A.at(i)+cumsum_a.at(i-1));
+        } else {
+            cumsum_a.pb(A.at(i));
+        }
+    }
+
+    rep(i, Q){
+        ull x = X.at(i);
+        if (x >= max_a){
+            cout << x*N - sum_a << endl;
+        } else if (x <= min_a) {
+            cout << sum_a - x*N << endl;
+        } else {
+            ll pos_up = upper_bound(A.begin(), A.end(), x) - A.begin();
+            ll pos_lo = lower_bound(A.begin(), A.end(), x) - A.begin();
+            ll l_sum = cumsum_a.at(pos_up);
+            ll r_sum = cumsum_a.at(N-1) - cumsum_a.at(pos_lo);
+            cout << l_sum + r_sum << endl;
+        }
+    }
+
+    return 0;
+}
