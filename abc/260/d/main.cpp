ba5736ffe4a19ee82e205ba4099324e7460c1c31
Daichi Yamazaki <yamazaki.d.zero@gmail.com>
2022-07-17 23:15:59 +0900

    Add ABC 260 A-D

---
+++ abc/260/d/main.cpp
@@ -0,0 +1,103 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+
+/* debug */
+// 標準エラー出力を含む提出はrejectされる場合もあるので注意
+#define debug(x) cerr << "\033[33m(line:" << __LINE__ << ") " << #x << ": " << x << "\033[m" << endl;
+
+/* func */
+inline int in_int() {int x; cin >> x; return x;}
+inline ll in_ll() {ll x; cin >> x; return x;}
+inline string in_str() {string x; cin >> x; return x;}
+// search_length: 走査するベクトル長の上限(先頭から何要素目までを検索対象とするか、1始まりで)
+template <typename T> inline bool vector_finder(std::vector<T> vec, T element, unsigned int search_length) {
+    auto itr = std::find(vec.begin(), vec.end(), element);
+    size_t index = std::distance( vec.begin(), itr );
+    if (index == vec.size() || index >= search_length) {return false;} else {return true;}
+}
+template <typename T> inline void print(const vector<T>& v, string s = " ")
+    {rep(i, v.size()) cout << v[i] << (i != (ll)v.size() - 1 ? s : "\n");}
+template <typename T, typename S> inline void print(const pair<T, S>& p)
+    {cout << p.first << " " << p.second << endl;}
+template <typename T> inline void print(const T& x) {cout << x << "\n";}
+template <typename T, typename S> inline void print(const vector<pair<T, S>>& v)
+    {for (auto&& p : v) print(p);}
+// 第一引数と第二引数を比較し、第一引数(a)をより大きい/小さい値に上書き
+template <typename T> inline bool chmin(T& a, const T& b) {bool compare = a > b; if (a > b) a = b; return compare;}
+template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b; if (a < b) a = b; return compare;}
+
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vi p(n);
+    rep(i, n) cin >> p[i];
+    map<int, vi> mng;
+    vi res(n);
+    rep(i, n) {
+        auto num = p[i];
+        auto lb = mng.lower_bound(num);
+        if(lb == mng.end()) {
+            if(k==1) {
+                res[num-1] = i+1;
+            }
+            else {
+                vi v = {num};
+                mng.emplace(num, v);
+            }
+        }
+        else {
+            if(lb->second.size()==k-1) {
+                res[num-1] = i+1;
+                for(const auto& x : lb->second) res[x-1] = i+1;
+                mng.erase(lb->first);
+            }
+            else {
+                lb->second.pb(num);
+                mng[num] = move(lb->second);  // without move, TLE!
+                // mng.insert(make_pair(num, lb->second));
+                mng.erase(lb->first);
+            }
+        }
+    }
+    rep(i, n) {
+        if(res[i]==0) res[i] = -1;
+    }
+    for (auto x : res) cout << x << endl;
+
+    return 0;
+}
+
